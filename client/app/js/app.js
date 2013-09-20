6f40b8e7306cba538fdfbeb9b4fb06c84c1049b8
campbel <Chris-Game@CAMPBEL.(none)>
2013-09-19 22:38:00 -0600

    adding token loading back.

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -7,10 +7,8 @@
 			$routeProvider.when('/', {templateUrl: 'views/home.html', controller: 'HomeCtrl'});
 			$routeProvider.otherwise({redirectTo: '/'});
 
-      /* Setting up XSRF
 			var token = document.getElementById("xsrftoken").value;
 			$http.defaults.headers.common["X-XSRF-token"] = token;
-      */
 		}]);
 }());
 
