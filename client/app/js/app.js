7aeda7541b2e6ca38c5de28088fb208ac536a0e2
Chris Campbell <chris15118@gmail.com>
2014-05-04 17:14:13 -0600

    remove all the example code... make this a true seed

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -3,7 +3,6 @@
 
 	angular.module('myApp', ['ngRoute', 'ngAnimate'])
 		.config(['$routeProvider', '$httpProvider', function($routeProvider, $http) {
-			$routeProvider.when('/', {templateUrl: 'views/todo.html', controller: 'TodoCtl'});
-      $routeProvider.otherwise({redirectTo: '/'});
+
 		}]);
 }());
